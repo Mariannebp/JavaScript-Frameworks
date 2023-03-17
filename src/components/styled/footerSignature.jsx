4fc7be6dfb0c46f3c558714a32f5aafa04a13987
Marianne Pedersen <mariannebpedersen@gmail.com>
2023-03-17 16:00:27 +0100

    deleted old unused styled files

--- src/components/styled/footerSignature.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-import Paragraph from "./paragraph";
-
-const FooterSignature = styled(Paragraph)`
-  margin-top: 30px;
-`;
-
-export default FooterSignature;
