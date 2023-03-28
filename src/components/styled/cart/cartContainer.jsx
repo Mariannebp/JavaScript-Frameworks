64a3dad5d077614aec88c71262bc726fd58a0fb5
Marianne Pedersen <mariannebpedersen@gmail.com>
2023-03-29 01:56:37 +0200

    Adjusting mediaQ on CartContainer

--- src/components/styled/cart/cartContainer.jsx
+++ src/components/styled/cart/cartContainer.jsx
@@ -2,4 +2,17 @@ import styled from "styled-components";
 
 export const CartContainer = styled.div`
   margin: 0 auto auto auto;
+  width: 522px;
+
+  @media (min-width: 431px) and (max-width: 523px) {
+    width: 422px;
+  }
+
+  @media (min-width: 340px) and (max-width: 430px) {
+    width: 320px;
+  }
+
+  @media (max-width: 339px) {
+    width: 232px;
+  }
 `;
