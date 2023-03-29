1fe2ec12af3f1fd72e4f7fbd4ce53e7eece33d50
Marianne Pedersen <mariannebpedersen@gmail.com>
2023-03-29 17:43:49 +0200

    Added simple 404 page

---
+++ src/components/styled/404/NotFoundContainer.jsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const NotFoundContainer = styled.div`
+  margin: 30px auto 50px auto;
+`;
---
+++ src/components/styled/404/index.jsx
@@ -0,0 +1,2 @@
+export { NotFoundHeading } from "./NotFoundHeading";
+export { NotFoundContainer } from "./NotFoundContainer";
